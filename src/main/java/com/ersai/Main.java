3e432b40f3a4ea9f1ae7357f76f64f9027518adf
y98423 <yangxiaobin@yangxiaobindeMacBook-Pro.local>
2016-04-18 00:52:53 +0800

    tijao

---
+++ src/main/java/com/ersai/Main.java
@@ -0,0 +1,10 @@
+package com.ersai;
+
+/**
+ * Created by yangxiaobin on 16/4/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
